388ca26c00862baa370c8e8422d0e00d18f032c0
Steve Lawson <156612338+UncleSte007@users.noreply.github.com>
2025-05-02 12:26:20 +0100

    Update cyclic.c
    added facility for non-zero start condition
    renamed 'yabs' to 'ya' for clarification that abs may not be called in future versions

--- cyclic.c
+++ cyclic.c
@@ -7,21 +7,27 @@
 	Thread model: posix
 */
 
-#define y range-abs(yabs)
+#define y end-abs(ya)
 #include <stdio.h>
 #include <stdlib.h>
 
 int step = 1;
-int range = 255;
-int yabs;
+int start = 20;
+int end = 200;
+int range = end - start;
+int ya;
 
 
 int main(){
     
-    for(yabs = -range; yabs <= range; yabs += step) // yabs: -255...0...+255
+    for(ya = -range; ya <= range; ya += step) // ya: -255...0...+255
     {        
         printf("value: %d\n", y);					//y: 0...255...0
+	    //other code goes here
+	    //....
+	    //....
     }    
     return 1;
 }
-
+//TODO: 1: allow fractions for step variable
+//	2: allow negative range, start and end values
